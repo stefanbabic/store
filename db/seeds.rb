4bae47090c57ce2f8a2007acb7576cee071c20d3
Stefan Babic <babic.stefan@gmail.com>
2017-01-03 00:57:58 +0100

    style product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create(title: 'Nike Air Pegasus',
+               description:
+               %{<p>
+                 Low shoes for every day.
+                </p>},
+                image_url: 'nike-AIR_PEGASUS.jpg',
+                price: 119.45)
+
+Product.create(title: 'Under Armour Jet Low',
+               description:
+               %{<p>
+                 Basketball shoes for great performance.
+                </p>},
+                image_url: 'under_armour-Jet_Low.jpg',
+                price: 69.95)
+
+Product.create(title: 'Adidas Superstar 80s',
+               description:
+               %{<p>
+                 Casual shoes for any style.
+                </p>},
+                image_url: 'adidas-SUPERSTAR_80s.jpg',
+                price: 99.95)
+
+Product.create(title: 'Jordan Trainer 1',
+               description:
+               %{<p>
+                 New basketball shoes from Michael Jordan.
+                </p>},
+                image_url: 'JORDAN_TRAINER_1.jpg',
+                price: 136.80)
