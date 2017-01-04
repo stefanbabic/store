2d677aa4c662e0ef4893a404c2957b94ac02a653
Stefan Babic <babic.stefan@gmail.com>
2017-01-04 14:54:24 +0100

    test controllers

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', 'Adidas Zero'
+    assert_select '.price', /\€[,\d]+\.\d\d/
   end
 
 end
