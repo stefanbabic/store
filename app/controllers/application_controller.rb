08e48fe9eda5974f1508f26e3072b16492b866cf
Stefan Babic <babic.stefan@gmail.com>
2017-01-06 14:05:53 +0100

    add item to cart with click on photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :time_loaded
+
+  private
+  def time_loaded
+    @time = Time.now.strftime("%H:%M - %d.%m.%Y")
+  end
 end
