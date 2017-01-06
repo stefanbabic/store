e74d3289e07ec89f4901ff91c7da6fe1814e3246
Stefan Babic <babic.stefan@gmail.com>
2017-01-06 15:22:52 +0100

    display counter of store visits

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = increment_counter
+    @message = "You've been here #{@count} times" if @count > 5
+  end
+
+  def increment_counter
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
