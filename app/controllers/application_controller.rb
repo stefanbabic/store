c95cc67af9a2645b20fde8e5ec31a4f397173d48
Stefan Babic <babic.stefan@gmail.com>
2017-01-16 14:02:40 +0100

    limit other users to login to admin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :time_loaded
+  before_action :authorize
 
   private
-  def time_loaded
-    @time = Time.now.strftime("%H:%M - %d.%m.%Y")
-  end
+
+    def time_loaded
+      @time = Time.now.strftime("%H:%M - %d.%m.%Y")
+    end
+
+  protected
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
+
 end
