f4c94dd64c822190104d9f6bd8d70dc8fb55bae2
Stefan Babic <babic.stefan@gmail.com>
2017-01-18 10:55:54 +0100

    translate the storefront

--- app/views/products/who_bought.atom.builder
+++ app/views/products/who_bought.atom.builder
@@ -19,13 +19,12 @@ atom_feed do |feed|
             xhtml.tr do
               xhtml.td item.product.title
               xhtml.td item.quantity
-              xhtml.td number_to_currency(item.total_price, unit: "€")
+              xhtml.td number_to_currency(item.total_price)
             end
           end
           xhtml.tr do
             xhtml.th 'total', colspan: 2
-            xhtml.th number_to_currency(order.line_items.map(&:total_price).sum, \
-              unit: "€")
+            xhtml.th number_to_currency(order.line_items.map(&:total_price).sum)
           end
         end
 
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,30 @@
+#encoding: utf-8
+
+LANGUAGES = [
+  ['English', 'en'],
+  ['Deutsch', 'de'],
+  ["Español".html_safe, 'es'],
+  ["français".html_safe, 'fr'],
+  ["русский".html_safe, 'ru'],
+  ['Nederlands', 'nl'],
+  ['Bosanski', 'bs'],
+  ["Български".html_safe, 'bg'],
+  ['dansk', 'da'],
+  ['Eesti', 'et'],
+  ['suomi', 'fi'],
+  ["Ελληνικά".html_safe, 'el'],
+  ['Italiano', 'it'],
+  ['hrvatski', 'hr'],
+  ["Latviešu".html_safe, 'lv'],
+  ["Lietuvių".html_safe, 'lt'],
+  ["Македонски".html_safe, 'mk'],
+  ['Norsk', 'nn'],
+  ["Português".html_safe, 'pt'],
+  ["Română".html_safe, 'ro'],
+  ['Svenska', 'sv'],
+  ["српски".html_safe, 'sr'],
+  ["slovenščina".html_safe, 'sl'],
+  ["Türkçe".html_safe, 'tr'],
+  ["українська".html_safe, 'uk'],
+  ['magyar', 'hu']
+]
